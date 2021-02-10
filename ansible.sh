9262bad96b515fbfef87b65becaca39e4a6ade20
venkatakrishna444 <venkatakrishna529@gmail.com>
2021-02-10 11:29:44 +0530

    added commits

---
+++ ansible.sh
@@ -0,0 +1,5 @@
+today were discuss about ansible guys
+keep calm and observe what im doing
+listen carefully
+keep practice
+
---
+++ jenkins.sh
@@ -0,0 +1,5 @@
+heyyy 
+this is very very impo concept in devops
+keep hardwork nd practice this topic 
+familier with this topic
+
